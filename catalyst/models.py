a9565c0549cb5b7721a668c5054a94e2cb9fc5ba
Nick Villa <njvilla1@gmail.com>
2014-10-26 15:32:03 -0400

    added lat and long to model

--- catalyst/models.py
+++ catalyst/models.py
@@ -5,7 +5,9 @@ class Profile(models.Model):
 	user = models.OneToOneField(User, null=True) 
 	info_blurb = models.CharField(max_length=250, default="default text")
 	photo = models.ImageField(upload_to='catalyst/profile_photos', null=True)
-	pub_date = models.DateTimeField('date published')	
+	pub_date = models.DateTimeField('date published')
+	latitude = models.DecimalField(max_digits=11, decimal_places=8)
+	longitude = models.DecimalField(max_digits=11, decimal_places=8)	
 	def __str__(self):
 		return str(self.pk) + '_' + str(self.user.username)
 	def prof_id(self):
